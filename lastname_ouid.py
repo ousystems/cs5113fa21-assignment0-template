294e034c6abc3f7ca31bac5a5dcac8b7128e573c
Christan Grant <christangrant@gmail.com>
2021-09-08 12:07:59 -0500

    added the template main program

---
+++ lastname_ouid.py
@@ -0,0 +1,17 @@
+from requests import get
+
+# URL = <assign value>
+# k = <assign value>
+
+def cipher_text():
+  # TODO fill in comments and content here
+  pass
+
+def plain_text():
+  # TODO fill in comments and content here
+  pass
+
+if __name__ == '__main__':
+    print(cipher_text())
+    print()
+    print(plain_text())
